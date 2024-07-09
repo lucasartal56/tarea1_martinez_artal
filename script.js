391e94353807bd7f7ab85742661c86e5f211bc3a
lucasartal56 <lucdanart@gmail.com>
2024-07-09 14:33:43 -0600

    Métodos String

---
+++ script.js
@@ -0,0 +1,11 @@
+let frase = 'Bienvenidos al mundo de la programación';
+
+// Método 1: split
+let palabras = frase.split(' ');
+console.log(palabras); // ["Bienvenidos", "al", "mundo", "de", "la", "programación"]
+
+// Método 2: toLowerCase
+console.log(frase.toLowerCase()); // "bienvenidos al mundo de la programación"
+
+// Método 3: charAt
+console.log(frase.charAt(5)); // "n"
