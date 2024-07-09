36fc865b375ee5eae86b27185a91b88055e0baf8
lucasartal56 <lucdanart@gmail.com>
2024-07-09 14:35:20 -0600

    Método Number

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+        //STRING
 let frase = 'Bienvenidos al mundo de la programación';
 
 // Método 1: split
@@ -9,3 +10,17 @@ console.log(frase.toLowerCase()); // "bienvenidos al mundo de la programación"
 
 // Método 3: charAt
 console.log(frase.charAt(5)); // "n"
+
+        //NUMBER
+let temperatura = 36.6;
+
+// Método 1: toPrecision
+console.log(temperatura.toPrecision(3)); // "36.6"
+
+// Método 2: isNaN
+let noNumero = "abc";
+console.log(isNaN(noNumero)); // true
+
+// Método 3: parseFloat
+let decimalString = "123.456";
+console.log(parseFloat(decimalString)); // 123.456
